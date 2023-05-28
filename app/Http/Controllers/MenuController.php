b5554e14d99c0be537c9a308706afc3192e7d8cd
unknown <zainulabdeen@techswivel.com>
2023-05-28 14:47:35 +0500

    All done

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -93,6 +93,7 @@ class MenuController extends BaseController
      */
 
     public function getMenuItems() {
+        return MenuItem::with('children')->get();
         throw new \Exception('implement in coding task 3');
     }
 }
--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,14 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function children()
+    {
+        return $this->hasMany('Childern', 'parent_id', 'id');
+    }
+
+    public function allChildren()
+    {
+        return $this->children()->with('allChildren');
+    }
+
 }
