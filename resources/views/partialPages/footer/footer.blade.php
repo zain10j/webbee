a184f3a22e7faddf2cf5016c45c8f0ca188a944a
unknown <zainulabdeen@techswivel.com>
2023-05-28 12:43:45 +0500

    Removed added code.

--- resources/views/partialPages/footer/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <footer class="main-footer no-print">
-        <div class="float-right d-none d-sm-block">
-            <b>{{ config('app.name', 'TechSwivel') }}</b>
-        </div>
-        <strong>Copyright &copy; {{date('Y')}} <a href="https://www.techswivel.com/" target="_blank">TechSwivel Pvt</a>.</strong> All rights reserved.
-    </footer>
-</div>
